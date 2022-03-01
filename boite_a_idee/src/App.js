526c020b32d573aca8ef917a51a04d9bc994a599
Honorable con <honorablecon@gmail.com>
2022-03-01 09:47:17 +0000

    add roouter

--- boite_a_idee/src/App.js
+++ boite_a_idee/src/App.js
@@ -1,18 +1,19 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Idees from "./pages/Idees";
 import Statistiques from "./pages/Statistiques";
 import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Header/>
       <Routes>
         <Route path='/' element={<Idees/>} />
-        <Route path='/Idees' element={<Idees/>} />
-        <Route path='/Statistiques' element={<Statistiques/>} />
+        <Route path='/idees' element={<Idees/>} />
+        <Route path='/statistiques' element={<Statistiques/>} />
       </Routes>
     </div>
   );
--- boite_a_idee/src/components/Header.js
+++ boite_a_idee/src/components/Header.js
@@ -4,14 +4,14 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 export default function Header(){
     return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="primary" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Boite idées</Navbar.Brand>
+                <Navbar.Brand href="#home" className="Brand">Boite idées</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Link to={"/Idees"}>idees</Link>
-                    <Link to={"/Statistiques"}>Statistiques"</Link>
+                <Nav className="m-auto">
+                    <Link to={"/idees"} className="Link">idees</Link>
+                    <Link to={"/statistiques"} className="Link">Statistiques</Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
